e78b3d936ebb66ca02a7f57f0fca683350cdd0b2
Jorge Lee <jrg.lee@gmail.com>
2016-08-23 10:41:14 -0700

    Remove RestTemplate setup. Set build scripts to use Java 8

--- requestbin/src/main/java/com/revinate/requestbin/RequestbinApplication.java
+++ requestbin/src/main/java/com/revinate/requestbin/RequestbinApplication.java
@@ -8,16 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
-import java.nio.charset.Charset;
-import java.util.List;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
-import static org.apache.commons.lang3.CharEncoding.UTF_8;
 
 @SpringBootApplication
 public class RequestbinApplication {
@@ -28,32 +20,13 @@ public class RequestbinApplication {
         .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-	public static void main(String[] args) {
-		SpringApplication.run(RequestbinApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RequestbinApplication.class, args);
+    }
 
     @Bean
     @Primary
     public ObjectMapper mapper() {
         return MAPPER;
     }
-
-    @Bean
-    @Primary
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-
-        List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();
-        converters.replaceAll(converter -> {
-            if (converter instanceof StringHttpMessageConverter) {
-                return new StringHttpMessageConverter(Charset.forName(UTF_8));
-            } else {
-                return converter;
-            }
-        });
-        converters.add(new MappingJackson2HttpMessageConverter(MAPPER));
-
-        return restTemplate;
-    }
-
 }
